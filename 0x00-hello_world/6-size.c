e940c5c8df97f8f4003ea4025b0434c55156f64f
DD-SQUARED <drewnare29@gmail.com>
2023-07-14 01:21:42 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,7 @@
  * main -Entry point
  * Return:always 0 (Success)
  */
+int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
