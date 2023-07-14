f7f7740c37f541929e9c72305874189b7f049778
DD-SQUARED <drewnare29@gmail.com>
2023-07-14 06:29:38 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * return: always 0
+ */
+int main(void)
+{
+char c;
+c= 'z';
+while(c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
