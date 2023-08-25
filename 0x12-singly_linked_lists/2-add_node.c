795c06915def1e27a0e6d53495f52eec84f2c594
DD-SQUARED <drewnare29@gmail.com>
2023-08-25 09:21:07 -0700

    update_9

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,14 +20,14 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 
 	if (!new)
-	
+
 		return (NULL);
-	
 
 	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
 	(*head) = new;
+	
 	}
 	return (*head);
 }
